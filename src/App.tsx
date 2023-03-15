dac7dbde6b12ceb76654a8494837b1bf4da96855
Conlley-Passarelli <89032459+Conlley-Passarelli@users.noreply.github.com>
2023-03-15 19:05:35 -0300

    Commit - 15/03/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
+import { MyNameIs } from './components/MyNameIs'
 
 export function App() {
   const [contador, setContador] = useState(0)
-
+ 
   return (
     <>
-    <button onClick={() => setContador(contador + 1)}>Contar + 1</button>
-    <h1>{contador}</h1>
+      <button onClick={() => setContador(contador + 1)}>Contar + 1</button>
+      <h1>{contador}</h1>
+      <MyNameIs name="Matheus" age={16} />
+      <MyNameIs name="Raul"  />
+      <MyNameIs name="Bruno" />
+      <MyNameIs name="Luan" />
     </>
   )
 }
