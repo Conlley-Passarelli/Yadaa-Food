91c858eff1ab5dbe6a065ee7dbada0f5a1064b6d
Conlley-Passarelli <89032459+Conlley-Passarelli@users.noreply.github.com>
2023-05-03 19:02:25 -0300

    Imported Styled-Componentes

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    width: 100%;
+    background-color: rgb(255, 0, 0); //red
+    height: 200px;
+    
+    h1 {
+        color: white;
+    }
+` 
